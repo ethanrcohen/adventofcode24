6c2a4304e54b371b6aaa9791d6a66cde12b97486
Ethan Cohen <ecohen56@gmail.com>
2024-12-05 14:02:53 -0500

    init

---
+++ lib/adventofcode24.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcode24 do
+  @moduledoc """
+  Documentation for `Adventofcode24`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode24.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
